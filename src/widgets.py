d382132bd633861cad66e6d6b50aaea62f25c876
FaZboi Soreq <fazbosoreq@gmail.com>
2020-01-10 14:22:46 +0200

    comment TreeObject + tab util functions and object

--- src/widgets.py
+++ src/widgets.py
@@ -17,57 +17,57 @@ class Popup(QWidget):
 
 
 
-class TreeFileWidget(QWidget):
-    def __init__(self, *args, **kwargs):
-        QWidget.__init__(self, *args, **kwargs)
-        hlay = QHBoxLayout(self)
-        self.treeview = QTreeView()
-        self.listview = QListView()
-        hlay.addWidget(self.treeview)
-        hlay.addWidget(self.listview)
+# class TreeFileWidget(QWidget):
+#     def __init__(self, *args, **kwargs):
+#         QWidget.__init__(self, *args, **kwargs)
+#         hlay = QHBoxLayout(self)
+#         self.treeview = QTreeView()
+#         self.listview = QListView()
+#         hlay.addWidget(self.treeview)
+#         hlay.addWidget(self.listview)
 
-        path = QDir.rootPath()
+#         path = QDir.rootPath()
 
-        self.dirModel = QFileSystemModel()
-        self.dirModel.setRootPath(QDir.rootPath())
-        self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
+#         self.dirModel = QFileSystemModel()
+#         self.dirModel.setRootPath(QDir.rootPath())
+#         self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
 
-        self.fileModel = QFileSystemModel()
-        self.fileModel.setFilter(QDir.NoDotAndDotDot |  QDir.Files)
+#         self.fileModel = QFileSystemModel()
+#         self.fileModel.setFilter(QDir.NoDotAndDotDot |  QDir.Files)
 
-        self.treeview.setModel(self.dirModel)
-        self.listview.setModel(self.fileModel)
+#         self.treeview.setModel(self.dirModel)
+#         self.listview.setModel(self.fileModel)
 
-        self.treeview.setRootIndex(self.dirModel.index(path))
-        self.listview.setRootIndex(self.fileModel.index(path))
+#         self.treeview.setRootIndex(self.dirModel.index(path))
+#         self.listview.setRootIndex(self.fileModel.index(path))
 
-        self.treeview.clicked.connect(self.on_clicked)
+#         self.treeview.clicked.connect(self.on_clicked)
 
-    app = QApplication(sys.argv)
-    # Splitter to show 2 views in same widget easily.
-    splitter = QSplitter()
-    # The model.
-    model = QFileSystemModel()
-    # You can setRootPath to any path.
-    model.setRootPath(QDir.rootPath())
-    # List of views.
-    views = []
-    for ViewType in (QColumnView, QTreeView):
-        # Create the view in the splitter.
-        view = ViewType(splitter)
-        # Set the model of the view.
-        view.setModel(model)
-        # Set the root index of the view as the user's home directory.
-        view.setRootIndex(model.index(QDir.homePath()))
-    # Show the splitter.
-    splitter.show()
-    # Maximize the splitter.
-    splitter.setWindowState(Qt.WindowMaximized)
+#     app = QApplication(sys.argv)
+#     # Splitter to show 2 views in same widget easily.
+#     splitter = QSplitter()
+#     # The model.
+#     model = QFileSystemModel()
+#     # You can setRootPath to any path.
+#     model.setRootPath(QDir.rootPath())
+#     # List of views.
+#     views = []
+#     for ViewType in (QColumnView, QTreeView):
+#         # Create the view in the splitter.
+#         view = ViewType(splitter)
+#         # Set the model of the view.
+#         view.setModel(model)
+#         # Set the root index of the view as the user's home directory.
+#         view.setRootIndex(model.index(QDir.homePath()))
+#     # Show the splitter.
+#     splitter.show()
+#     # Maximize the splitter.
+#     splitter.setWindowState(Qt.WindowMaximized)
 
 
-    def on_clicked(self, index):
-        path = self.dirModel.fileInfo(index).absoluteFilePath()
-        self.listview.setRootIndex(self.fileModel.setRootPath(path))
+#     def on_clicked(self, index):
+#         path = self.dirModel.fileInfo(index).absoluteFilePath()
+#         self.listview.setRootIndex(self.fileModel.setRootPath(path))
 
 
 
@@ -77,12 +77,26 @@ class TabWidget(QTabWidget):
         self.parent = parent
         self.textEdit = QTextEdit(self.parent)
         self.textEdit.setGeometry(QRect(180, 40, 1650, 850))
-
+        
+    def create_tab(self, name: str = "tab", closable: bool = True):
+        tab = Tab()
+        tab.setObjectName(name)
+        self.addTab(self.tab, "")
 
     def removeTab(self, index):
         widget = self.widget(index)
         if widget is not None:
             widget.deleteLater()
         self.removeTab(index)
+
+
+class Tab(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self.parent = parent
+        self.textEdit = QTextEdit(self.parent)
+        self.textEdit.setGeometry(QRect(180, 40, 1650, 850))    
     
+    def __str__(self):
+        return self.textEdit.toPlainText()
         
