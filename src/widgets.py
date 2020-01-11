d75aae40afbd7303ebaf2b23342245bce49c1766
FaZboi Soreq <fazbosoreq@gmail.com>
2020-01-11 22:30:36 +0200

    Reused code , TreeFileWidget created

--- src/widgets.py
+++ src/widgets.py
@@ -11,62 +11,23 @@ from utils import DIR_ICON_PATH, FILE_ICON_PATH
 class Popup(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-    
 
 
 
 
-# class TreeFileWidget(QWidget):
-#     def __init__(self, *args, **kwargs):
-#         QWidget.__init__(self, *args, **kwargs)
-#         hlay = QHBoxLayout(self)
-#         self.treeview = QTreeView()
-#         self.listview = QListView()
-#         hlay.addWidget(self.treeview)
-#         hlay.addWidget(self.listview)
 
-#         path = QDir.rootPath()
-
-#         self.dirModel = QFileSystemModel()
-#         self.dirModel.setRootPath(QDir.rootPath())
-#         self.dirModel.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
-
-#         self.fileModel = QFileSystemModel()
-#         self.fileModel.setFilter(QDir.NoDotAndDotDot |  QDir.Files)
-
-#         self.treeview.setModel(self.dirModel)
-#         self.listview.setModel(self.fileModel)
-
-#         self.treeview.setRootIndex(self.dirModel.index(path))
-#         self.listview.setRootIndex(self.fileModel.index(path))
-
-#         self.treeview.clicked.connect(self.on_clicked)
-
-#     app = QApplication(sys.argv)
-#     # Splitter to show 2 views in same widget easily.
-#     splitter = QSplitter()
-#     # The model.
-#     model = QFileSystemModel()
-#     # You can setRootPath to any path.
-#     model.setRootPath(QDir.rootPath())
-#     # List of views.
-#     views = []
-#     for ViewType in (QColumnView, QTreeView):
-#         # Create the view in the splitter.
-#         view = ViewType(splitter)
-#         # Set the model of the view.
-#         view.setModel(model)
-#         # Set the root index of the view as the user's home directory.
-#         view.setRootIndex(model.index(QDir.homePath()))
-#     # Show the splitter.
-#     splitter.show()
-#     # Maximize the splitter.
-#     splitter.setWindowState(Qt.WindowMaximized)
+class TreeFileWidget(QTreeWidget):
+    def __init__(self, parent = None):
+        QTreeWidget.__init__(self, parent)
+        self.clicked.connect(self.on_double_clicked)
+        self.clicks = 0
 
 
-#     def on_clicked(self, index):
-#         path = self.dirModel.fileInfo(index).absoluteFilePath()
-#         self.listview.setRootIndex(self.fileModel.setRootPath(path))
+    def on_double_clicked(self, index):
+        if self.clicks >= 2:
+            
+        QtGui.QTreeWidget.mousePressEvent(self, event)
+
 
 
 
