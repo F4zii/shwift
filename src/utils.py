8b3f81153c5f727f443df07f0e01a43c186f1249
FaZboi Soreq <fazbosoreq@gmail.com>
2020-01-13 16:02:28 +0200

    Stylesheet toggle

--- src/utils.py
+++ src/utils.py
@@ -90,6 +90,26 @@ def get_file_name(curr_file):
 
     return os.path.join(here, str(curr_file))
 
+
+
+def toggle_stylesheet(path):
+    '''
+    Toggle the stylesheet to use the desired path in the Qt resource
+    system (prefixed by `:/`) or generically (a path to a file on
+    system).
+
+    :path:      A full path to a resource or file on system
+    '''
+
+    # get the QApplication instance,  or crash if not set
+    app = QApplication.instance()
+    if app is None:
+        raise RuntimeError("No Qt Application found.")
+
+    file = QFile(path)
+    file.open(QFile.ReadOnly | QFile.Text)
+    stream = QTextStream(file)
+    app.setStyleSheet(stream.readAll())
     
 # def initDirectory(self, path):
 #     new_item = newItem(path)
