479bcdccc18751db4b125476ff41c72a961e69db
FaZboi Soreq <fazbosoreq@gmail.com>
2020-01-19 21:14:29 +0200

    properties and fields names

--- src/threads.py
+++ src/threads.py
@@ -6,14 +6,15 @@ from utils import get_file_list
 
 class TreeViewUpdateThread(QThread):
 
-    def __init__(self):
+    def __init__(self, dirname):
+        self._dirname = dirname
         self.tree_view_modified = pyqtSignal()
-        self.file_list = get_file_list('.')
+        self.file_list = get_file_list(self._dirname)
 
     def run(self):
         while True:
-            updated_file_list = get_file_list('.')
+            updated_file_list = get_file_list(self._dirname)
             if file_list != updated_file_list:
                 self.tree_view_modified.emit()
                 self.file_list = updated_file_list
-            time.sleep(3)
+            time.sleep(1.5)
