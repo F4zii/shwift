406d83df6cf357651d849ab29320dec6c5f91e7c
FaZboi Soreq <fazbosoreq@gmail.com>
2020-01-19 21:15:07 +0200

    Editor Major Methods

--- src/widgets/ui_widgets.py
+++ src/widgets/ui_widgets.py
@@ -23,9 +23,16 @@ class Popup(QWidget):
 
 
 class Editor(QPlainTextEdit):
-    def __init__(self, parent=None):
-        QTextEdit.__init__(self, parent)
+    def __init__(self):
+        super().__init__(self, parent)
         self.setGeometry(QRect(180, 40, 1650, 850))
+        self.lineNumberArea = LineNumberArea(self)
+
+        self.connect(self, SIGNAL('blockCountChanged(int)'), self.updateLineNumberAreaWidth)
+        self.connect(self, SIGNAL('updateRequest(QRect,int)'), self.updateLineNumberArea)
+        self.connect(self, SIGNAL('cursorPositionChanged()'), self.highlightCurrentLine)
+
+        self.updateLineNumberAreaWidth(0)
 
     def clear_text():
         self.textEdit.setText("")
@@ -39,3 +46,139 @@ class Editor(QPlainTextEdit):
             self.textEdit.setAlignment(Qt.AlignRight | Qt.AlignAbsolute)
         elif action == self.actionAlignJustify:
             self.textEdit.setAlignment(Qt.AlignJustify)
+
+
+    def lineNumberAreaWidth(self):
+        digits = 1
+        count = max(1, self.blockCount())
+        while count >= 10:
+            count /= 10
+            digits += 1
+        space = 3 + self.fontMetrics().width('9') * digits
+        return space
+
+
+    def updateLineNumberAreaWidth(self, _):
+        self.setViewportMargins(self.lineNumberAreaWidth(), 0, 0, 0)
+
+
+    def updateLineNumberArea(self, rect, dy):
+
+        if dy:
+            self.lineNumberArea.scroll(0, dy)
+        else:
+            self.lineNumberArea.update(0, rect.y(), self.lineNumberArea.width(),
+                       rect.height())
+
+        if rect.contains(self.viewport().rect()):
+            self.updateLineNumberAreaWidth(0)
+
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        cr = self.contentsRect();
+        self.lineNumberArea.setGeometry(QRect(cr.left(), cr.top(),
+                    self.lineNumberAreaWidth(), cr.height()))
+
+
+    def lineNumberAreaPaintEvent(self, event):
+        mypainter = QPainter(self.lineNumberArea)
+
+        mypainter.fillRect(event.rect(), Qt.lightGray)
+
+        block = self.firstVisibleBlock()
+        blockNumber = block.blockNumber()
+        top = self.blockBoundingGeometry(block).translated(self.contentOffset()).top()
+        bottom = top + self.blockBoundingRect(block).height()
+
+        # Just to make sure I use the right font
+        height = self.fontMetrics().height()
+        while block.isValid() and (top <= event.rect().bottom()):
+            if block.isVisible() and (bottom >= event.rect().top()):
+                number = str(blockNumber + 1)
+                mypainter.setPen(Qt.black)
+                mypainter.drawText(0, top, self.lineNumberArea.width(), height,
+                 Qt.AlignRight, number)
+
+            block = block.next()
+            top = bottom
+            bottom = top + self.blockBoundingRect(block).height()
+            blockNumber += 1
+
+
+    def highlightCurrentLine(self):
+        extraSelections = []
+
+        if not self.isReadOnly():
+            selection = QTextEdit.ExtraSelection()
+
+            lineColor = QColor(Qt.yellow).lighter(160)
+
+            selection.format.setBackground(lineColor)
+            selection.format.setProperty(QTextFormat.FullWidthSelection, True)
+            selection.cursor = self.textCursor()
+            selection.cursor.clearSelection()
+            extraSelections.append(selection)
+        self.setExtraSelections(extraSelections)
+
+
+class NumberBar(QWidget):
+    def __init__(self, parent = None):
+        super(NumberBar, self).__init__(parent)
+        self.editor = parent
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.editor.blockCountChanged.connect(self.update_width)
+        self.editor.updateRequest.connect(self.update_on_scroll)
+        self.update_width('1')
+
+    def update_on_scroll(self, rect, scroll):
+        if self.isVisible():
+            if scroll:
+                self.scroll(0, scroll)
+            else:
+                self.update()
+
+    def update_width(self, string):
+        width = self.fontMetrics().width(str(string)) + 10
+        if self.width() != width:
+            self.setFixedWidth(width)
+
+    def paintEvent(self, event):
+        if self.isVisible():
+            block = self.editor.firstVisibleBlock()
+            height = self.fontMetrics().height()
+            number = block.blockNumber()
+            painter = QPainter(self)
+            painter.fillRect(event.rect(), lineBarColor)
+            painter.drawRect(0, 0, event.rect().width() - 1, event.rect().height() - 1)
+            font = painter.font()
+
+            current_block = self.editor.textCursor().block().blockNumber() + 1
+
+            condition = True
+            while block.isValid() and condition:
+                block_geometry = self.editor.blockBoundingGeometry(block)
+                offset = self.editor.contentOffset()
+                block_top = block_geometry.translated(offset).top()
+                number += 1
+
+                rect = QRect(0, block_top, self.width() - 5, height)
+
+                if number == current_block:
+                    font.setBold(True)
+                else:
+                    font.setBold(False)
+
+                painter.setFont(font)
+                painter.drawText(rect, Qt.AlignRight, '%i'%number)
+
+                if block_top > event.rect().bottom():
+                    condition = False
+
+                block = block.next()
+
+            painter.end()
+
+
