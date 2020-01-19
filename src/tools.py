e5ab931447001115e5ea1339025fc008205fce61
FaZboi Soreq <fazbosoreq@gmail.com>
2020-01-19 21:30:48 +0200

    Testing files

--- src/tools.py
+++ src/tools.py
@@ -15,6 +15,39 @@ from PyQt5.QtCore import (
     QPoint,
     QSize,
 )
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+
+
+class OpenFolderDialog(object):
+    def _open_file_dialog(self):
+        directory = str(QtWidgets.QFileDialog.getExistingDirectory())
+        self.lineEdit.setText('{}'.format(directory))
+
+    def _set_text(self, text):
+        return text
+
+    def setupUi(self, TestQFileDialog):
+        TestQFileDialog.setObjectName("TestQFileDialog")
+        TestQFileDialog.resize(240, 320)
+
+        self.toolButtonOpenDialog = QtWidgets.QToolButton(TestQFileDialog)
+        self.toolButtonOpenDialog.setGeometry(QtCore.QRect(210, 10, 25, 19))
+        self.toolButtonOpenDialog.setObjectName("toolButtonOpenDialog")
+        self.toolButtonOpenDialog.clicked.connect(self._open_file_dialog)
+
+        self.lineEdit = QtWidgets.QLineEdit(TestQFileDialog)
+        self.lineEdit.setEnabled(False)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 10, 191, 20))
+        self.lineEdit.setObjectName("lineEdit")
+
+        self.retranslateUi(TestQFileDialog)
+        QtCore.QMetaObject.connectSlotsByName(TestQFileDialog)
+
+    def retranslateUi(self, TestQFileDialog):
+        _translate = QtCore.QCoreApplication.translate
+        TestQFileDialog.setWindowTitle(_translate("TestQFileDialog", "Dialog"))
+        self.toolButtonOpenDialog.setText(_translate("TestQFileDialog", "..."))
 
 
 class Terminal(QPlainTextEdit):
