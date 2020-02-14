2e4a211e7e3baaf6c0a56adfedc2834e81e9d733
F4zii <Basketball780237209>
2020-02-14 19:21:43 +0200

    Layout setup at main.py

--- shwift/main.py
+++ shwift/main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
+    ui.setLayout(ui.mainLayout)
     theme_file = QFile(":/dark.qss")
     theme_file.open(QFile.ReadOnly | QFile.Text)
     stream = QTextStream(theme_file)
